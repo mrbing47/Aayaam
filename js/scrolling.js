51607acf031a9c8abde4bf95bb165c28faca5332
Sarthik <sarthikgarg01@gmail.com>
2019-10-02 20:46:06 +0530

    added media query for navbar

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,4 +1,4 @@
-window.onload=()=>{
+window.onload = () => {
 	const navbar = document.querySelector('.navbar');
 	const intro_img = document.querySelector('.intro-div');
 
@@ -8,15 +8,15 @@ window.onload=()=>{
 
 	const observer = new IntersectionObserver(function(entries, observer) {
 		entries.forEach(entry => {
-			if(!entry.isIntersecting){
-            navbar.classList.add("navbar-scrolled");
-        }
-        else{
-            navbar.classList.remove("navbar-scrolled");
-        }
-			console.log(entry.target);
+			if (entry.rootBounds.width > 768) {
+				if (!entry.isIntersecting) {
+					navbar.classList.add('navbar-scrolled');
+				} else {
+					navbar.classList.remove('navbar-scrolled');
+				}
+			}
 		});
 	}, options);
 
 	observer.observe(intro_img);
-}
+};
